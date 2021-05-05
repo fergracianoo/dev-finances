250506a883c149111b00908c6dff87cb564a331b
Fernando Graciano <fernandoggraciano@gmail.com>
2021-05-05 20:19:41 -0300

    chore: create transactions array

--- scripts.js
+++ scripts.js
@@ -12,3 +12,36 @@ const Modal = {
     .remove('active')
   }
 }
+
+const transactions = [
+  {
+    id: 1,
+    description: 'Luz',
+    amount: -50000,
+    date: '23/01/2021',
+  },
+  {
+    id: 2,
+    description: 'Website',
+    amount: 500000,
+    date: '23/01/2021',
+  },
+  {
+    id: 3,
+    description: 'Internet',
+    amount: -20000,
+    date: '23/01/2021',
+  },
+]
+
+const Transaction = {
+  incomes() {
+    
+  },
+  expenses() {
+
+  },
+  total() {
+
+  }
+}
