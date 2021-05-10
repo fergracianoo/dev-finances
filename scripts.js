d00a04ff4492b4e71201b6b0785321562a2853e2
Fernando Graciano <fernandoggraciano@gmail.com>
2021-05-10 14:07:21 -0300

    chore: add formatCurrency function

--- scripts.js
+++ scripts.js
@@ -32,6 +32,12 @@ const transactions = [
     amount: -20000,
     date: '23/01/2021',
   },
+  {
+    id: 4,
+    description: 'App',
+    amount: 200000,
+    date: '23/01/2021',
+  },
 ]
 
 const Transaction = {
@@ -45,3 +51,42 @@ const Transaction = {
 
   }
 }
+
+const DOM = {
+  transactionsContainer: document.querySelector('#data-table tbody'),
+  addTransaction(transaction, index) {
+    const tr = document.createElement('tr')
+    tr.innerHTML = DOM.innerHTMLTransaction(transaction)
+    DOM.transactionsContainer.appendChild(tr)
+  },
+  innerHTMLTransaction(transaction) {
+    const CSSclass = transaction.amount > 0 ? "income" : "expense"
+    const amount = Utils.formatCurrency(transaction.amount)
+    const html = `
+      <td class="description">${transaction.description}</td>
+      <td class="${CSSclass}">${amount}</td>
+      <td class="date">${transaction.date}</td>
+      <td>
+        <img src="./assets/minus.svg" alt="Remover transação">
+      </td>
+    `
+    return html
+  }
+}
+
+const Utils = {
+  formatCurrency(value) {
+    const signal = Number(value) < 0 ? "-" : ""
+    value = String(value).replace(/\D/g, "")
+    value = Number(value) / 100
+    value = value.toLocaleString("pt-BR", {
+      style: "currency",
+      currency: "BRL"
+    })
+    return signal + value
+  }
+}
+
+transactions.forEach(function(transaction) {
+  DOM.addTransaction(transaction)
+})
